d27b598651e499c8a4af3c50aebfb519d94e8ac7
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-11-17 12:04:49 +0000

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1151,7 +1151,7 @@ CAmount GetBlockSubsidy(int nPrevBits, int nPrevHeight, const Consensus::Params&
     double dDiff;
     CAmount nSubsidyBase;
 
-    nSubsidyBase = 50;
+    nSubsidyBase = 25;
 
     CAmount nSubsidy = nSubsidyBase * COIN;
 
